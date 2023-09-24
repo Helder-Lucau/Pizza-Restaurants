6e94371aa1144b0fed1b21bf5fe4cc29c07bdd3d
Helder-Lucau <helderlucau@gmail.com>
2023-09-25 00:35:25 +0300

    Generating migrations

---
+++ server/api/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Api
+from flask_migrate import Migrate
+
+from api.models import db
+
+# create the app and configure the SQLite database
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizzaria.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+# Import the routes to avoid circular importation 
+from api import routes
---
+++ server/api/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+# class Restaurant(db.Model, SerializerMixin):
+#     __tablename__ = 'restaurants'
+
+
+    
