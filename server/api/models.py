45c7273e15497ac237eae509b261accba806d358
Helder-Lucau <helderlucau@gmail.com>
2023-09-25 01:48:25 +0300

    added pizza table

--- server/api/models.py
+++ server/api/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -17,4 +18,16 @@ class Restaurant(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f'Name={self.name} Address={self.address}'
+
+class Pizza(db.Model, SerializerMixin):
+     __tablename__ = 'pizzas'
+
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String)
+     ingredients = db.Column(db.String)
+     created_at = db.Column(db.DateTime, server_default=db.func.now())
+     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+     def __repr__(self):
+        return f'Name={self.name} Ingredients={self.ingredients}'
     
