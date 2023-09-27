33d806921cb459874a04dcc81c2938965fc3892f
Helder-Lucau <helderlucau@gmail.com>
2023-09-27 12:01:54 +0300

    post http method to insert restaurantpizzas record into the db

--- server/api/routes.py
+++ server/api/routes.py
@@ -28,7 +28,7 @@ class Restaurants(Resource):
 api.add_resource(Restaurants, '/restaurants')
 
 class RestaurantByID(Resource):
-
+    # get the record using the id 
     def get(self, id):
 
         restaurant = Restaurant.query.filter_by(id=id).first()
@@ -46,7 +46,8 @@ class RestaurantByID(Resource):
                 200
             )
             return response
-    
+        
+    # delete the record using the id and returns a message
     def delete(self, id):
 
         restaurant_by_id = Restaurant.query.filter_by(id=id).first()
@@ -66,7 +67,7 @@ class RestaurantByID(Resource):
                 200
                 )            
         return response
-    
+# add the resource to the API
 api.add_resource(RestaurantByID, '/restaurants/<int:id>')
 
 class Pizzas(Resource):
@@ -81,25 +82,36 @@ class Pizzas(Resource):
         )
         return response
     
+# add the resource to the API   
 api.add_resource(Pizzas, '/pizzas')
     
 class RestaurantPizzas(Resource):
+
     def post(self):
-        user_data = request.get_json()
-        new_restaurant_pizza = RestaurantPizza(
-            price=user_data['price'],
-            pizza_id=user_data['pizza_id'],
-            restaurant_id=user_data['restaurant_id']
-        )
-        db.session.add(new_restaurant_pizza)
-        db.session.commit()
+        try:
+            user_data = request.get_json()
+            new_restaurant_pizza = RestaurantPizza(
+                price=user_data['price'],
+                pizza_id=user_data['pizza_id'],
+                restaurant_id=user_data['restaurant_id']
+            )
+            db.session.add(new_restaurant_pizza)
+            db.session.commit()
+            pizza = Pizza.query.filter(Pizza.id==user_data['pizza_id']).first()
+            pizza_dict = {
+                "id": pizza.id,
+                "name": pizza.name,
+                "ingredients": pizza.ingredients
+            }
+            response = make_response(jsonify(pizza_dict), 201)
+            return response
+        except ValueError as e:
+            response = make_response({"errors": e.args}, 400)
+            return response
+        except Exception as e:
+            response = make_response({"errors": e.args}, 400)
+            return response
 
-        # if not RestaurantPizza.query.filter(RestaurantPizza.id == new_restaurant_pizza.id).first():
-        #     response_body
-
-        response_dict = new_restaurant_pizza.to_dict()
-        response = make_response(jsonify(response_dict), 201)
-        return response
 # add the resource to the API
 api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
